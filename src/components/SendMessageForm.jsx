ce827e9c30bb5d131d2501d63aa3bc76e65ff59f
pilar <sauleturme@gmail.com>
2018-10-02 18:05:46 +0200

    created the handleSubmit method to  fetch the messages and send them off

--- src/components/SendMessageForm.jsx
+++ src/components/SendMessageForm.jsx
@@ -11,19 +11,32 @@ class SendMessageForm extends Component{
     this.state = {
       message: ''
     }
+    //.bind change the value of this
+    //no we have access to the this keyword inside of it
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(e) {
+    //this method updates the state to whatever the user types into the input field
     this.setState({
       message: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.sendMessage(this.state.message)
+    //we’re calling the sendMessage prop and passing in this.state.message as a parameter
+    this.setState({
+      message: ''
     })
   }
 
   render(){
     return(
       <Fragment>
-        <form className="input-group my-3">
+        <form className="input-group my-3" onSubmit={this.handleSubmit}>
           <div class="input-group-prepend">
             <span class="input-group-text"> <FontAwesomeIcon icon={faSmile}/></span>
           </div>
